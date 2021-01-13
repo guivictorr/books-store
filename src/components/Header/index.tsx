c77e15d92a3c1a403fbe892d75fd8cd989212909
guivictorr <guilhermevictor20@gmail.com>
2021-01-13 00:11:29 -0300

    feat(header): search button enable text input instead title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 import { useNavigation } from '@react-navigation/native';
 import { HeaderProps } from '../../interfaces';
 
-import { HeaderContainer, HeaderTitle } from './styles';
+import { HeaderContainer, HeaderTitle, HeaderInput } from './styles';
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
+  const [isSearching, setIsSearching] = useState(false);
+  const [inputText, setInputText] = useState('');
   const { goBack } = useNavigation();
+
   return (
     <HeaderContainer>
       <BorderlessButton onPress={() => goBack()}>
         <Feather name="arrow-left" size={25} color="#0f0f0f" />
       </BorderlessButton>
-      <HeaderTitle>{title}</HeaderTitle>
-      <BorderlessButton>
-        <Feather name="search" size={25} color="#0f0f0f" />
+      {isSearching ? (
+        <HeaderInput
+          placeholder="What are you searching ?"
+          value={inputText}
+          onChangeText={text => setInputText(text)}
+        />
+      ) : (
+        <HeaderTitle>{title}</HeaderTitle>
+      )}
+      <BorderlessButton onPress={() => setIsSearching(!isSearching)}>
+        <Feather
+          name={isSearching ? 'check' : 'search'}
+          size={25}
+          color="#0f0f0f"
+        />
       </BorderlessButton>
     </HeaderContainer>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -13,3 +13,13 @@ export const HeaderTitle = styled.Text`
   font-size: 26px;
   font-family: 'Ubuntu_400Regular';
 `;
+
+export const HeaderInput = styled.TextInput`
+  background-color: #f5f5f5;
+  flex: 1;
+  margin: 0 20px;
+  font-family: 'Ubuntu_400Regular';
+  font-size: 22px;
+  padding: 8px;
+  border-radius: 4px;
+`;
